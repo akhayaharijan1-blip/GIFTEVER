83d145ca1f52e37fe9dee0a1263d978a4919d4c2
GIFTEVER <akhayaharijan1@gmail.com>
2026-06-30 17:13:58 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("GIFTEVER Loaded Successfully");
+window.addEventListener("scroll", function () {
+
+    const navbar = document.querySelector(".navbar");
+
+    if (window.scrollY > 50) {
+
+        navbar.classList.add("scrolled");
+
+    } else {
+
+        navbar.classList.remove("scrolled");
+
+    }
+
+});
