5e41be8fbce237afd11f193c0689b4f813d382d2
GIFTEVER <akhayaharijan1@gmail.com>
2026-07-01 20:17:37 +0530

    Update script.js

--- script.js
+++ script.js
@@ -14,3 +14,24 @@ window.addEventListener("scroll", function () {
     }
 
 });
+const fadeElements = document.querySelectorAll(".fade-up");
+
+const observer = new IntersectionObserver((entries) => {
+
+entries.forEach((entry) => {
+
+if(entry.isIntersecting){
+
+entry.target.classList.add("show");
+
+}
+
+});
+
+},{
+
+threshold:0.15
+
+});
+
+fadeElements.forEach((el)=>observer.observe(el));
